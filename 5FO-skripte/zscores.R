4d238ea4ae4247dd3c693aa05dbb9043fbf0dc51
EdgarRandLivid <s2daradl@uni-trier.de>
2018-01-30 19:58:55 +0100

    execute Z-score-Transformation on detected faces (500) using R

---
+++ 5FO-skripte/zscores.R
@@ -0,0 +1,18 @@
+##Normalisierung von Gesichtern: Z-Score-Transformation
+
+#Einlesen der Datei
+f <-read.csv (file="4FE-009.csv", sep=",")
+
+#Darstellung der Daten als Tabelle
+f 
+
+# Normalisierung: Z-score-Transformation
+# auf 4 Stellen runden
+zscore = round((f$faces- mean(f$faces))/(sd(f$faces)),4)
+zscore
+
+#zscore zu Tabelle hinzufuegen
+f$zscore_faces =  zscore 
+
+#csv-Datei speichern
+write.table(f, file = "7FO-011.csv",row.names=FALSE,col.names=TRUE, sep=",", dec=".")
